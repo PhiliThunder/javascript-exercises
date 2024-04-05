688447488ad83a42fd3038ef9b04f52782d16018
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-05 17:21:06 +0200

    All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = '';
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
+    if (num < 0) return "ERROR";
+    return word;
 };
 
 // Do not edit below this line
