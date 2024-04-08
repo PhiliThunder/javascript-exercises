e3701ec8dad4ee6dbddd8231ab937680e92f3b69
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-08 19:43:18 +0200

    Reviewed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
 const removeFromArray = function(array, ...remove) {
     let filteredArray = array;
-    for (const value of remove) {
+    for (const value of remove) {                                     
         filteredArray = filteredArray.filter(item => item !== value);
     }
     return filteredArray;
+    /*
+    Could have simply used includes() instead of the custom loop, something like this:
+    return array.filter(val => !remove.includes(val))
+    */
 };
 
 // Do not edit below this line
