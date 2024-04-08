8c684ad6d7385d3c02de399313552160e358a2c4
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-08 20:12:15 +0200

    Works when smallest number is first argument

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = num1 + num2;
+    let difference = num2 - num1;
+    for (let i = difference; i > num1; i--) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
