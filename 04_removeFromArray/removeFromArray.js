583d3cdadad2d1ea742380da0c74436eb9588d75
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-08 18:42:03 +0200

    passes all required tests, but does not scale

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function(array, remove) {
-    return array.filter(item => item !== remove);
+const removeFromArray = function(array, remove, remove2, remove3, remove4) {
+    return array.filter(item => item !== remove 
+        && item !== remove2 
+        && item !== remove3 
+        && item !== remove4);
 };
 
 // Do not edit below this line
