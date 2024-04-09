5ee9c308ac3b56a431ca3037db278bb67def0bb5
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-09 17:51:58 +0200

    Reviewed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -9,6 +9,11 @@ const leapYears = function(year) {
         return isLeapYear;
     }
     return isLeapYear;
+    /*
+    Could have been done in one line like this: return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    - No designated variable needed as the result of the expression will return true or false by itself.
+    - All logic checks can be done in one line using parantheses.
+    */
 };
 
 // Do not edit below this line
