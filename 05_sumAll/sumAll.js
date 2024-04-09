c226724631cfefdcd7e7928dc6f5bb6141d7cf77
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-09 16:48:35 +0200

    All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,25 @@
-const sumAll = function(num1, num2) {
+const sumAll = function (num1, num2) {
     let sum = num1 + num2;
-    let difference = num2 - num1;
-    for (let i = difference; i > num1; i--) {
-        sum += i;
+    let difference = 0;
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (num1 < num2) {
+        difference = num2 - num1;
+        for (let i = difference; i > num1; i--) {
+            sum += i;
+        }
+    } else {
+        difference = num1 - num2;
+        for (let i = difference; i > num2; i--) {
+            sum += i;
+        }
     }
     return sum;
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
