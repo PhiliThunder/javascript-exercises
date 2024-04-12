69b8ec74bc1a52bcba71b619b0b14d4ddd985350
Philip Dahlberg <philiparonkd@gmail.com>
2024-04-12 16:09:12 +0200

    Reviewed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,7 +7,13 @@ const convertToFahrenheit = function(celsius) {
   let toFahrenheit = (celsius * (9 / 5) + 32);
   return Math.round(toFahrenheit * 10) / 10;
 };
+/*Review:
+The conversions could have been done in one line,
+without the extra varibles, however i found it to be
+more readable and nicer like this.
 
+Parantheses are really important to put in the right places when using such functions and formulas.
+*/
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
