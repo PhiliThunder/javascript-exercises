f609c4ea93827982b6bcb9d45d866f71013242e1
Philip Dahlberg <philiparonkd@gmail.com>
2024-05-01 20:14:13 +0200

    Reviewed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
 const palindromes = function (string) {
     const proccessed = string.toLowerCase().split(" ").join("").split("");
     const filtered = proccessed.filter(symbol => symbol != "," && symbol != "!" && symbol != ".");
+    //If i had created a varible with all valid characters, i could have just checked with .includes() within the .filter()
+    //to remove not only these symbols but also spaces at the same time, meaning i also would not have had to do the unecessary .split and .join
     const processedOriginal = filtered.join("");
     const reversedResult = filtered.reverse().join("");
     if (reversedResult == processedOriginal) {
