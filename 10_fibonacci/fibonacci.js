67114d3196114876615e03804255039aae268b74
Philip Dahlberg <philiparonkd@gmail.com>
2024-05-01 21:04:23 +0200

    All tests passed, i am fine with solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    fiboNum = Number(num);
+    if (fiboNum == 0) {return 0};
+    if (fiboNum < 0) {return "OOPS"};
+    if (fiboNum == 1) {return 1};
+    let firstNum = 0;
+    let secondNum = 1;
+    let result = firstNum + secondNum;
+    for (let i = 2; i <= fiboNum; i++) {
+        result = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
