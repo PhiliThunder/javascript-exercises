009f6626e1b08b1860697e64bc36a21550ff29b5
Philip Dahlberg <philiparonkd@gmail.com>
2024-05-01 20:05:41 +0200

    Tests passed, but i don't like my "forced" solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const proccessed = string.toLowerCase().split(" ").join("").split("");
+    const filtered = proccessed.filter(symbol => symbol != "," && symbol != "!" && symbol != ".");
+    const processedOriginal = filtered.join("");
+    const reversedResult = filtered.reverse().join("");
+    if (reversedResult == processedOriginal) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
