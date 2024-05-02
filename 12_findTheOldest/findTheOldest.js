59cc7bceaf467670a2beffc40f377941026ed8de
Philip Dahlberg <philiparonkd@gmail.com>
2024-05-02 19:50:35 +0200

    Tests passed, but confused, see comment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.reduce((highObj, currObj) => {
+        if (!currObj.yearOfDeath) { currObj.yearOfDeath = new Date().getFullYear() }
+        return (currObj.yearOfDeath - currObj.yearOfBirth) > (highObj.yearOfDeath - highObj.yearOfBirth)
+            ? currObj : highObj;
+    }, arr[0]);//initialValue has to be specified so the !yearOfDeath check gets done on the first object of the array. (i believe)
+               //Otherwise highObj is just going to be set to arr[0] without running that arr element through the reduction function first.
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+new Date().getFullYear()
